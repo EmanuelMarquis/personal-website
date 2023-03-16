ed481e5065353893975ae2eae304aa642a8fdeb4
Emanuel Marquis <eman0dev@gmail.com>
2023-03-16 13:39:54 -0300

    feat: added navbar

---
+++ src/components/navbar.jsx
@@ -0,0 +1,10 @@
+
+export default function Navbar() {
+    return (<nav className="fixed top-0 w-full flex justify-between main-color border-b-4 text-3xl">
+        <input className="border-r-4 border-color px-6 navbar-btn" type="button" value="EM" onClick={function (){console.log("EM")}}/>
+        <span>
+            <input className="border-l-4 py-6 px-6 navbar-btn" type="button" value="portfolio" onClick={function (){console.log("portfolio")}}/>
+            <input className="border-l-4 py-6 px-6 navbar-btn" type="button" value="contact" onClick={function (){console.log("contact")}}/>
+        </span>
+    </nav>);
+}
