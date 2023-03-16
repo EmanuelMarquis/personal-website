420fe43b25dcfe80ed8b2988444b260fdc6c23f6
Emanuel Marquis <eman0dev@gmail.com>
2023-03-16 14:11:46 -0300

    feat: added floating window shadow

--- src/App.js
+++ src/App.js
@@ -1,25 +1,29 @@
-import logo from './logo.svg';
 import './App.css';
+import Navbar from './components/navbar';
+import FloatingWindow from './components/floatingWindow';
+import { useEffect } from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+export default function App() {
+  
+  useEffect(function () {
+    document.title = "Emanuel Marquis";
+  }, []);
+
+  const aboutMe = <text>
+    I'm a 18 year-old lad from the<br/>
+    northeast of Brazil, looking for<br/>
+    cool job opportunities as a<br/> 
+    frontend developer.
+  </text>;
+
+  return (<div>
+    <Navbar/>
+    <p className='font-bold text-center text-6xl my-48'>
+      Hello! I'm Emanuel Marquis.<br/>
+      I'm a Frontend Developer.
+    </p>
+    <div className='flex justify-end pr-12'>
+      <FloatingWindow msg={aboutMe}/>
     </div>
-  );
+  </div>);
 }
-
-export default App;
--- src/components/floatingWindow.jsx
+++ src/components/floatingWindow.jsx
@@ -4,14 +4,16 @@
  * @returns 
  */
 export default function FloatingWindow({msg}) {
-    return (<div className="border-4 inline-block green text-3xl">
-        <div className="flex py-2 pr-2 border-b-4 justify-end gap-2">
-            <span className="floating-window-dot"/>
-            <span className="floating-window-dot"/>
-            <span className="floating-window-dot"/>
-        </div>
-        <div className="pt-8 pb-6 px-6">
-            {msg}
+    return (<div className="inline-block floating-window-shadow text-3xl">
+        <div className="relative border-4 green" style={{top: "-10px", left: "-10px"}}>
+            <div className="flex py-2 pr-2 border-b-4 justify-end gap-2">
+                <span className="floating-window-dot"/>
+                <span className="floating-window-dot"/>
+                <span className="floating-window-dot"/>
+            </div>
+            <div className="pt-8 pb-6 px-6">
+                {msg}
+            </div>
         </div>
     </div>);
 }
