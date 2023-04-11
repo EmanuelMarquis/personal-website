52d282e90d2cea175df182c562f36102d0ef931a
Emanuel Marquis <eman0dev@gmail.com>
2023-04-11 15:20:00 -0300

    refactor: cleaner code

--- src/components/floatingWindow.jsx
+++ src/components/floatingWindow.jsx
@@ -3,14 +3,19 @@
  * @param {React.SVGProps<SVGTextElement>} msg
  * @returns 
  */
-export default function FloatingWindow({msg}) {
-    return (<div className="inline-block floating-window-shadow text-3xl">
+export default function FloatingWindow({msg, title, isPosAbsolute}) {
+    const titleBar = <>
+        <p className="text-xl font-bold ml-4 mr-16">{title}</p>
+        <span className="flex gap-2">
+            <span className="floating-window-dot"/>
+            <span className="floating-window-dot"/>
+            <span className="floating-window-dot"/>
+        </span>
+    </>
+        
+    return (<div className={`${isPosAbsolute === true? "absolute" : ""} inline-block floating-window-shadow text-3xl`}>
         <div className="relative border-4 green" style={{top: "-10px", left: "-10px"}}>
-            <div className="flex py-2 pr-2 border-b-4 justify-end gap-2">
-                <span className="floating-window-dot"/>
-                <span className="floating-window-dot"/>
-                <span className="floating-window-dot"/>
-            </div>
+            <div className="flex py-2 text-sml pr-2 border-b-4 justify-between ">{titleBar}</div>
             <div className="pt-8 pb-6 px-6">{msg}</div>
         </div>
     </div>);
