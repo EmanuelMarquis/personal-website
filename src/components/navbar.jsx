cd8723097ce076439e58346dbe814cdb542e0681
Emanuel Marquis <eman0dev@gmail.com>
2023-04-18 16:51:35 -0300

    added logo btn

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -6,24 +6,22 @@ export default function Navbar() {
     return (<nav className="fixed z-10 top-0 w-full flex justify-between main-color border-b-4 text-3xl">
         <LogoBtn/>
         <span>
-            <PortifolioBtn navigate={navigate}/>
-            <ContactBtn navigate={navigate}/>
+            <PortifolioBtn navigate={() => navigate("/portifolio")}/>
+            <ContactBtn navigate={() => navigate("/contact")}/>
         </span>
     </nav>);
 }
 
 function LogoBtn() {
-    return <input className="border-r-4 border-color px-6 navbar-btn" type="button" value="EM" onClick={() => window.location.reload()}/>
-
+    const logo = <img alt="" width={"82rem"} src={require("../assets/logo.png")}/>
+    return <button className="border-r-4 border-color px-6 navbar-btn" onClick={() => window.location.reload()}>{logo}</button>
 }
 
 function PortifolioBtn({navigate}) {
-    const handleClick = () => navigate("/portifolio");
-    return <input className="border-l-4 py-6 px-6 navbar-btn" type="button" value="portfolio" onClick={handleClick}/>
+    return <input className="border-l-4 py-6 px-6 navbar-btn" type="button" value="portfolio" onClick={navigate}/>
 }
 
 function ContactBtn({navigate}) {
-    const handleClick = () => navigate("/contact");
-    return <input className="border-l-4 py-6 px-6 navbar-btn" type="button" value="contact" onClick={handleClick}/>
+    return <input className="border-l-4 py-6 px-6 navbar-btn" type="button" value="contact" onClick={navigate}/>
 }
 
