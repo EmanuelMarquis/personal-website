fceb5d77339178a623b59a4a25e89edd1704ad9a
Emanuel Marquis <eman0dev@gmail.com>
2023-04-27 08:49:45 -0300

    refactor: cleaner code

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -7,15 +7,17 @@ export default function Navbar() {
     return (<nav className="fixed z-10 top-0 w-full flex justify-between main-color border-b-4 text-3xl">
         <LogoBtn/>
         <span>
-            <PortifolioBtn navigate={() => navigate("/portifolio")}/>
+            <PortifolioBtn navigate={() => navigate("/portfolio")}/>
             <ContactBtn navigate={() => navigate("/contact")}/>
         </span>
     </nav>);
 }
 
 function LogoBtn() {
-    const img = <img alt="" width={"82rem"} src={logo}/>
-    return <button className="border-r-4 border-color px-6 navbar-btn" onClick={() => window.location.reload()}>{img}</button>
+    return <button className="border-r-4 border-color px-6 navbar-btn" 
+    onClick={() => window.location.reload()}>
+        <img alt="" width={"82rem"} src={logo}/>
+    </button>
 }
 
 function PortifolioBtn({navigate}) {
